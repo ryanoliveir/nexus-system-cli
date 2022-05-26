189ce434332e33d42f6dfc9c9820a39531ef5084
macorrea <ma-xlo@debian.core>
2022-05-26 18:30:39 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    FILE *fp = fopen("./arts/menu.txt", "r");
+    char linha[100];
+    char opcao;
+    
+    system("clear");
+    while(fgets(linha, 100, fp) != NULL)
+        printf("%s", linha);
+    printf("\n");
+}
