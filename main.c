f1168db694cc08e9a7c03f784524ac66ac368166
Barbara Lopes <barbaralopes@MacBook-Pro-de-Barbara.local>
2022-06-05 18:23:58 -0300

    adicionei um printf

--- main.c
+++ main.c
@@ -6,11 +6,13 @@ void main()
     FILE *fp = fopen("./arts/menu.txt", "r");
     char linha[100];
     char opcao;
-    
+
     system("clear");
     while(fgets(linha, 100, fp) != NULL)
         printf("%s", linha);
     printf("\n");
 
     printf("Teste git 2");
+
+    printf("Barbarinha");
 }
